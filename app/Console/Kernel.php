b0f0bf55784a98ad9bfd2e6fb55fb8e235795720
Oğuzhan Hayrandere <hayrandere1@hotmail.com>
2024-01-01 23:16:55 +0300

    schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,37 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $port = env('PUSHER_PORT', 6001);
+        $path = str_replace(DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'Console', '', __DIR__);
+
+        $schedule->call(function () use ($path) {
+            shell_exec("php " . $path . "/artisan websockets:restart &");
+        })->dailyAt('03:00');
+
+        $schedule->call(function () use ($path) {
+            $runningPids = shell_exec("pgrep -fl \"" . $path . "/artisan queue:work\"");
+            $explode = explode(' php', $runningPids);
+            if (count($explode) < 2) {
+//                $schedule->command('queue:work --daemon');
+                shell_exec("php " . $path . DIRECTORY_SEPARATOR . "artisan queue:work > " . $path . "/storage/logs/laravel_queue.log &");
+            }
+        })->everyFiveMinutes();
+
+        $schedule->call(function () use ($path) {
+            $runningPids = shell_exec("pgrep -fl \"" . $path . DIRECTORY_SEPARATOR . "artisan queue:work --queue=csv_generate\"");
+            $explode = explode(' php', $runningPids);
+            if (count($explode) < 2) {
+                shell_exec("php " . $path . DIRECTORY_SEPARATOR . "artisan queue:work --queue=csv_generate > " . $path . "/storage/logs/laravel_queue.log &");
+            }
+        })->everyFiveMinutes();
+        $schedule->call(function () use ($path, $port) {
+            $running_pids = shell_exec("pgrep -fl \"" . $path . DIRECTORY_SEPARATOR . "artisan websocket:serve\"");
+            $explode = explode(' php', $running_pids);
+            if (count($explode) < 2) {
+                shell_exec("php " . $path . DIRECTORY_SEPARATOR . "artisan websocket:serve --host=0.0.0.0 --port=" . $port . " > " . $path . "/storage/logs/laravel_websocket.log &");
+            }
+        })->everyFiveMinutes();
+        $schedule->command('telescope:prune')->daily();
     }
 
     /**
